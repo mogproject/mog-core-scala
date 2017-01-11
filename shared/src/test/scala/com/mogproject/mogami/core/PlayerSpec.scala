620f95e248733d0368860d645509969b5b390916
Yosuke Mizutani <mogproj@gmail.com>
2017-01-11 15:46:22 -0500

    implemented Player#toSymbolString

--- shared/src/test/scala/com/mogproject/mogami/core/PlayerSpec.scala
+++ shared/src/test/scala/com/mogproject/mogami/core/PlayerSpec.scala
@@ -28,6 +28,11 @@ class PlayerSpec extends FlatSpec with MustMatchers with GeneratorDrivenProperty
     WHITE.toSfenString must be("w")
   }
 
+  "Player#toSymbolString" must "describe unicode characters" in {
+    BLACK.toSymbolString must be("☗")
+    WHITE.toSymbolString must be("☖")
+  }
+
   "Player#parseCsaString" must "make player" in {
     Player.parseCsaString("+") must be(Some(BLACK))
     Player.parseCsaString("-") must be(Some(WHITE))
