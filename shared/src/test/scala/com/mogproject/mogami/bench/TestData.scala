40a11fbf0985c2e23cce54a8b5e020c4e9870680
Yosuke Mizutani <mogproj@gmail.com>
2017-01-15 18:56:51 -0500

    added benchmark

--- shared/src/test/scala/com/mogproject/mogami/bench/TestData.scala
+++ shared/src/test/scala/com/mogproject/mogami/bench/TestData.scala
@@ -10,4 +10,17 @@ trait TestData {
     " 3e2d 3c2d 2h2d P*2c 2d2f 7d7e P*8c 7b8c B*6c B*7d",
     " 6c1h+ 7d4g+ 4i5h 4g1d"
   ).mkString
+  val recordSfen03: String = Seq(
+    "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 0",
+    " 7g7f 8c8d 8h7g 3c3d 7i6h 4a3b 6g6f 3a4b 6h6g 4b3c",
+    " 2h8h 2b3a 5i4h 5a4a 4h3h 7a7b 3h2h 5c5d 1i1h 1c1d",
+    " 2h1i 6a5b 3i2h 4c4d 4i3i 5b4c 6f6e 3c2d 4g4f 2a3c",
+    " 6g5f 6c6d 4f4e 4d4e 6e6d 3a6d 8h6h P*6c P*4d 4c4b",
+    " 5f6e 6d5e 6e5d 5e7g+ 8i7g 4e4f 6i5h B*4g 5h4g 4f4g+",
+    " B*7a G*3h 7a8b+ 3h2h 6h2h 3c2e R*7a P*5a B*6b 4a3a",
+    " 7a5a+ S*4a 8b7b 4g3g 2h9h 3g2g 7b6c P*5b 4d4c+ 2e3g+",
+    " 4c4b 3b4b 2i3g 2g3g P*2h P*4c P*3b 3a3b P*4d 4c4d",
+    " 6b4d+ 2d3c G*4c 4b4c 5d4c+ 3b2b 4d3c 2b1c N*2e 1c1b",
+    " G*2b"
+  ).mkString
 }
